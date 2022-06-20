fcd35dc72e971205b11c37d467e228c0894e7ee8
Shyamin Ayesh <me@shyamin.com>
2022-06-20 15:37:46 +0530

    simple routing mechanisam

--- router.go
+++ router.go
@@ -1,21 +1,57 @@
 package tgbcr
 
-import "fmt"
+import (
+	"strings"
+)
 
-type Router struct{}
-type Context struct{}
-type Handler func(Context)
+type Router struct {
+	Collection struct {
+		Childs []Child
+	}
+}
+type Handler func()
+type Child struct {
+	Path    string
+	Handler Handler
+}
 
 func New() *Router {
 	return &Router{}
 }
 
-func (r *Router) Handle(path string, handle Handler) {
+func (r *Router) Handle(path string, handler Handler) {
 
-	fmt.Println(path)
+	if path == "" {
+		panic("Path must not be empty")
+	}
+
+	if path[0] != '/' {
+		panic("Path must start with \"/\"")
+	}
+
+	if len(path) < 2 {
+		panic("Path must have at least single charactor to match")
+	}
+
+	r.Collection.Childs = append(r.Collection.Childs, Child{
+		Path:    path,
+		Handler: handler,
+	})
 
 }
 
-func (r *Router) Dispatch() {
-	fmt.Println("Dispatch")
+func (r *Router) Dispatch(text string) {
+
+	/**
+	* Sanitize and extract the command string from
+	* the provided text string
+	 */
+	path := strings.Fields(text)[0]
+	for _, e := range r.Collection.Childs {
+		if e.Path == path {
+			e.Handler()
+			return
+		}
+	}
+
 }
--- router_test.go
+++ router_test.go
@@ -1,11 +1,13 @@
 package tgbcr
 
-import "testing"
+import (
+	"testing"
+)
 
-func Test_New(t *testing.T) {
+func Test_Dispatch(t *testing.T) {
 
 	router := New()
-	router.Handle("/hello", func(c Context) {})
-	router.Dispatch()
+	router.Handle("/hello", func() {})
+	router.Dispatch("/hello params")
 
 }
